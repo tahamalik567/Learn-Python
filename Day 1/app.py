2a6c5d5ce6ae93ed0a5989829bd8525d6326d98e
tahamalik567 <tahamalik567@gmail.com>
2024-03-09 20:44:07 +0500

    save

---
+++ Day 1/app.py
@@ -0,0 +1,7 @@
+print("hello world!!")
+# this is string of charracter 
+
+
+print("A 'single quote' inside a double quote")
+print('A "double quote" inside a single quote')
+print("Alternatively you can just \"escape\" the quote")
