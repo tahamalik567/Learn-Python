045058ef8548133cec3d582a5fca6d97aa96204f
Muhammad Taha Malik <tahamalik567@gmail.com>
2024-03-10 20:20:24 +0500

    update

---
+++ Day 1/Coding Exercise Input Function.py
@@ -0,0 +1,23 @@
+# Instructions
+# Write a program that calculates and outputs the number of characters in any name. The automated tests will try out lots of different names as the input. Your code should work for any name. Your code should only output the number, no other text is needed.
+
+# Hint
+# Remember, you can use len() around any piece of text to calculate the number of characters.
+
+# e.g. https://www.google.com/search?q=how+to+get+the+length+of+a+string+in+python+stack+overflow
+
+# Important
+# Don't add prompt text to the input() function.
+
+# e.g. use
+
+# ✅ name = input()
+
+# don't use:
+
+# ❌ name = input("What's your name?")
+
+name = input()
+print(len(name))
+# or 
+print(len(input()))
--- Day 1/app.py
+++ Day 1/app.py
@@ -5,3 +5,11 @@ print("hello world!!")
 print("A 'single quote' inside a double quote")
 print('A "double quote" inside a single quote')
 print("Alternatively you can just \"escape\" the quote")
+
+# String Manipulation and Code Intelligence
+print("this is how to move to next line \n by using escape character")
+#string concatination
+print("Hello" + "World")
+print("hello" + " " + "world")
+
+print("Hello \n" * 3)
