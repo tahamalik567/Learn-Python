ed9cd37d5308dad6fd9ce54520ac81bb9d42a621
Muhammad Taha Malik <tahamalik567@gmail.com>
2024-03-10 22:42:22 +0500

    updates

---
+++ Day 1/python variables.py
@@ -0,0 +1,10 @@
+name = input("what is youe name?")
+print(name)
+# name is a varable and input is a funtion
+name = "john"
+name = "angela"
+print(name)
+print("asdf" + str(len(name)))
+# so you can only print string not integer with concatination
+print("asdf" + len(name)) 
+# TypeError: can only concatenate str (not "int") to str
