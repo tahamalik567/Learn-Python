3f37a46a1a1aa23782fbe290767e5f255faf34d4
tahamalik567 <tahamalik567@gmail.com>
2024-03-22 22:34:22 +0500

    logical op

--- Day 3/Nested if statements.py
+++ Day 3/Nested if statements.py
@@ -6,6 +6,8 @@ if(height > 120):
         print("Please pay $5")
     elif(age <= 18):
         print("Please pay $7")
+    elif(age >= 45 and age <= 55):
+        print("Please pay $0, have a free ride")
     else:
         print("Please pay $12")
 else:
